53c4117af184e6632e869b3119e4008a9e8fcc44
akidon0000 <a@art.jp>
2023-04-02 15:28:44 +0900

    グリッド表示

--- flutter_practice/lib/image_list.dart
+++ flutter_practice/lib/image_list.dart
@@ -5,10 +5,19 @@ class ImageList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+
+    final images = [
+      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+    ];
+
     return Scaffold(
       appBar: AppBar(title: const Text('タイトル')),
-      body: Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+      body: GridView.count(crossAxisCount: 2, children: images),
     );
   }
 }
