815920b5398ba55e6988775e800c8c6f24fc2464
akidon0000 <a@art.jp>
2023-04-02 15:32:33 +0900

    ピンチアウトピンチイン

--- flutter_practice/lib/image_list.dart
+++ flutter_practice/lib/image_list.dart
@@ -5,19 +5,13 @@ class ImageList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final images = [
-      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
-      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
-      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
-      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
-      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
-      Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
-    ];
-
     return Scaffold(
       appBar: AppBar(title: const Text('タイトル')),
-      body: GridView.count(crossAxisCount: 2, children: images),
+      body: InteractiveViewer(
+        child:
+            Image.network('https://tech.mof-mof.co.jp/_nuxt/img/63792f2.png'),
+      ),
     );
   }
 }
+
