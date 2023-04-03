f7c87a31af6d5d3cd8d0e12d00d77dadadbbbdaa
akidon0000 <a@art.jp>
2023-04-04 00:07:12 +0900

    エラー削除

--- flutter_practice/lib/view/settings.dart
+++ flutter_practice/lib/view/settings.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class SettingsPage extends StatefulWidget {
+    // コンストラクタに名前付き 'key' パラメータを追加
+  const SettingsPage({Key? key}) : super(key: key);
+
   @override
   _SettingsPageState createState() => _SettingsPageState();
 }
@@ -10,19 +13,22 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
+      navigationBar:const CupertinoNavigationBar(
         middle: Text('設定'),
       ),
       child: SafeArea(
         child: ListView(
           children: <Widget>[
-            SizedBox(height: 12),
+            const SizedBox(height: 12),
+            _buildSettingsItem("Password", CupertinoIcons.wifi),
+
+            const SizedBox(height: 12),
             _buildSettingsSection(title: "一般", items: [
               _buildSettingsItem("Wi-Fi", CupertinoIcons.wifi),
               _buildSettingsItem("Bluetooth", CupertinoIcons.bluetooth),
               _buildSettingsItem("モバイルネットワーク", CupertinoIcons.antenna_radiowaves_left_right),
             ]),
-            SizedBox(height: 12),
+            const SizedBox(height: 12),
             _buildSettingsSection(title: "個人用", items: [
               _buildSettingsItem("通知", CupertinoIcons.bell),
               _buildSettingsItem("サウンド", CupertinoIcons.speaker_3),
@@ -56,7 +62,7 @@ class _SettingsPageState extends State<SettingsPage> {
       color: CupertinoColors.white,
       child: Column(
         children: [
-          SizedBox(height: 8),
+          const SizedBox(height: 8),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Row(
@@ -65,16 +71,16 @@ class _SettingsPageState extends State<SettingsPage> {
                 Row(
                   children: [
                     Icon(icon, size: 24, color: CupertinoColors.systemBlue),
-                    SizedBox(width: 12),
-                    Text(title, style: TextStyle(fontSize: 14, color: CupertinoColors.systemBlue)),
+                    const SizedBox(width: 12),
+                    Text(title, style:const TextStyle(fontSize: 14, color: CupertinoColors.black)),
                   ],
                 ),
-                Icon(CupertinoIcons.right_chevron, size: 24, color: CupertinoColors.systemBlue),
+                const Icon(CupertinoIcons.right_chevron, size: 24, color: CupertinoColors.systemGrey),
               ],
             ),
           ),
-          SizedBox(height: 8),
-          Divider(height: 1, thickness: 1, color: CupertinoColors.systemGrey4),
+          const SizedBox(height: 8),
+          const Divider(height: 1, thickness: 1, color: CupertinoColors.systemGrey4),
         ],
       ),
     );
